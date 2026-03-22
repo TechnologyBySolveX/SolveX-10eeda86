8736bcdd2c84937d15753635051c13b4c70b89b9
Sunil-731820 <sunilg@infinite.com>
2026-03-22 12:38:23 +0530

    After login user profile Page will show

--- solvexstreamfrontend/src/App.js
+++ solvexstreamfrontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './features/auth/Home';
 import Login from './features/auth/Login';
 import Signup from './features/auth/Signup';
+import Profile from './features/auth/Profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
